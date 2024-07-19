2561117cea5509f4a7aa7f029c9d274426acbd01
TobiasB00n <157970882+TobiasB00n@users.noreply.github.com>
2024-07-19 18:39:24 +0300

    10

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ import path from 'path';
 import { Bot, InlineKeyboard, Keyboard,InputFile } from "grammy";
 import { fileURLToPath } from 'url';
 import apiRoutes from './routes/api.js';
+import crypto from "crypto";
+import { bot } from "./js/bot.js";
 
+const app = express();
+const port = 3000;
 
+app.use(express.json());
 
 bot.command("start", (ctx) => ctx.reply(`hello`))
 bot.start()
 console.log('goodbye');
-const app = express();
-const port = 3000;
-import { bot } from "./js/bot.js";
-app.use(express.json());
+
+
+
 app.use('/api', apiRoutes);
 app.use('/css', express.static('css'));
 app.use('/js', express.static('js'));
@@ -25,3 +29,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 
 });
+console.log(ctx.message.text);
